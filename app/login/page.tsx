536e4af67a5a97863ad15ae33ebb850437e72339
ShivamDosso21Com <shivam.srivastavadosso21@gmail.com>
2024-08-05 16:37:12 +0530

    push for test

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,17 @@ import LoginForm from "./LoginForm";
 
 export default function Login() {
     return (
-      <>
-        <div className="bg-white w-1/2 h-1/3 rounded">
+      <div className="">
+      <div className="flex items-center justify-center mt-64">
+        <div className="bg-white w-1/5 h-1/3 rounded-3xl shadow shadow-lg shadow-gray-800 p-5">
             <div className="">
               <div className="text-center text-color text-3xl font-bold pt-5">Skill On Time</div>
-              <div className="text-center text-gray-500 font-bold">Login</div>
+              <div className="text-center text-gray-500 font-bold">Student Login</div>
               <LoginForm />
             </div>
         </div>
-      </>
+      </div>
+      </div>
     );
   }
   
