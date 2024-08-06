a910a48092d5700c8f9cc2d4b3962bf0370ce177
vikram singh <vikramsinghkhinchi5@gmail.com>
2024-08-06 11:28:00 +0530

    Added new changes

--- app/Link/RightContent.tsx
+++ app/Link/RightContent.tsx
@@ -6,13 +6,17 @@ interface RightContentProps {
   lessons: string[];
 }
 
-const RightContent: React.FC<RightContentProps> = ({ lessons  }) => {
+const RightContent: React.FC<RightContentProps> = ({ lessons }) => {
   return (
-    <div className="w-full md:w-3/4 pl-4">
+    <div className="w-full md:w-3/4  box-border p-2 border-2 m-2 mt-9">
+      <div className='text-black font-bold'>Lessons</div>
       <ul>
         {lessons.map((lesson) => (
-          <li key={lesson} className="border-b border-gray-200 py-2 flex justify-between">
-            <span>{lesson}</span>
+          <li key={lesson} className="border-b border-gray-200 py-2 flex justify-between items-center">
+            <span className="flex items-center">
+              <span className="mr-2 text-blue-500">📖</span>
+              {lesson}
+            </span>
             <span className="text-yellow-500">⚠️</span>
           </li>
         ))}
